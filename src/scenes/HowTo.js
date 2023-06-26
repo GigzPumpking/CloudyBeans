b2e5c3be3ae572aca50d80fbf9484934c2087fa7
Gigz <gigzpumpking@gmail.com>
2023-06-26 06:49:40 -0700

    How to page filled and fixed, pause UI adjusted

--- src/scenes/HowTo.js
+++ src/scenes/HowTo.js
@@ -19,9 +19,21 @@ class HowTo extends Phaser.Scene {
 
         this.add.text(game.config.width/2, game.config.height/6, 'How To Play', menuConfig).setOrigin(0.5);
 
+        this.add.text(game.config.width/2, game.config.height/3, 'Click Beans', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/3 + 75, 'Click UFOs', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/3 + 150, 'Click Upgrades', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/3 + 225, 'Have Fun!', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/3 + 300, 'Press ESC to return to the menu', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/3 + 375, 'Press P to pause', menuConfig).setOrigin(0.5);
+
+
+        keyESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+
     }
 
     update() {
-
+        if (Phaser.Input.Keyboard.JustDown(keyESC)) {
+            this.scene.start('titleScene');    
+        }
     }
 }
